517096ddc1c9b25d808b2369a3c90de22f56d3d4
ionwolf13 <voli.scorp13@gmail.com>
2020-09-30 13:49:11 -0500

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  while array[counter] do
+    array[counter] = array[counter]**2
+    counter += 1
+  end
+  return array
 end
+
+array1 = [1,2,3]
+array2 = [9,10,16,25]
+square_array(array1)
+square_array(array2)
